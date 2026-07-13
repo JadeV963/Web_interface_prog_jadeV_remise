7e44c705e3fc9e5c906caea9918af1d689db0710
shippo24 <jade.valdor.1@ulaval.ca>
2026-07-12 22:02:04 -0400

    Adding rendering odule for artist cards

---
+++ lab19/ui.js
@@ -0,0 +1,17 @@
+export function renderArtists(artists, container) {
+    container.innerHTML = "";
+
+    artists.forEach((artist) => {
+        const card = document.createElement("artist-card");
+
+        card.setAttribute("data-id", artist.id);
+        card.setAttribute("name", artist.name);
+        card.setAttribute("genre", artist.genre);
+        card.setAttribute("stage", artist.stage);
+        card.setAttribute("time",artist.time);
+        card.setAttribute("country", artist.country);
+
+        container.appendChild(card);
+        });
+    
+}
